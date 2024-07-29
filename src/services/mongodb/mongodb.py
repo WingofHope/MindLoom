ddce026849e3d78acaaa13e65c465a093f2112c0
msjpurf <msjpurf@gmail.com>
2024-07-28 21:21:03 -0700

    config added TEMPLATE_LOAD_METHOD & TEMPLATE_FILE_PATH mongodb addded new feature authSource. added more env supply & error handle

--- src/services/mongodb/mongodb.py
+++ src/services/mongodb/mongodb.py
@@ -3,18 +3,20 @@
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 import time
+import os
 
 from ...config import MONGO_CONFIG
 from ..logger.base_logger import BaseLogger
 
 class MongoDB:
     def __init__(self):
-        self.logger = BaseLogger('mongodb')
+        self.logger = BaseLogger('mongodb').get_logger()
         self.host = MONGO_CONFIG['host']
         self.port = MONGO_CONFIG['port']
         self.username = MONGO_CONFIG['username']
         self.password = MONGO_CONFIG['password']
         self.db_name = MONGO_CONFIG['db_name']
+        self.auth_source = MONGO_CONFIG.get('auth_source')
         self.client = None
         self.db = None
         self.connect_attempts = 3
@@ -24,7 +26,13 @@ class MongoDB:
         attempt = 1
         while attempt <= self.connect_attempts:
             try:
-                self.client = MongoClient(host=self.host, port=self.port, username=self.username, password=self.password)
+                self.client = MongoClient(
+                    host=self.host, 
+                    port=self.port, 
+                    username=self.username, 
+                    password=self.password,
+                    authSource=self.auth_source
+                )
                 self.db = self.client[self.db_name]
                 self.logger.info("Connected to MongoDB successfully!")
                 return
@@ -35,26 +43,34 @@ class MongoDB:
                     return
                 attempt += 1
                 time.sleep(2 ** attempt)  # Exponential backoff
+            except Exception as e:
+                self.logger.error(f"Unexpected error: {e}")
+                return
 
     def get_collection(self, collection_name):
         return self.db[collection_name]
     
     def insert_one(self, collection_name, document):
-        collection = self.get_collection(collection_name)
-        result = collection.insert_one(document)
-        self.logger.info(f"Inserted document with id {result.inserted_id}")
-        return result.inserted_id
+        try:
+            collection = self.get_collection(collection_name)
+            result = collection.insert_one(document)
+            self.logger.info(f"Inserted document with id {result.inserted_id}")
+            return result.inserted_id
+        except Exception as e:
+            self.logger.error(f"Failed to insert document: {e}")
+            return
 
     def find_one(self, collection_name, query):
         if self.client is None:
             self.logger.info("Not connected to MongoDB! Attempting to reconnect...")
             self.connect()
 
-        collection = self.db[collection_name]
         try:
+            collection = self.db[collection_name]
             result = collection.find_one(query)
             return result
         except Exception as e:
             self.logger.error(f"Failed to find document: {e}")
+            return None
 
 mongo_db = MongoDB()
