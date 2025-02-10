5253bce6dbce7b9f7fafd55507dfb366993e2fe5
laiyuanxiang <carterlai91@gmail.com>
2025-02-10 21:43:14 +0800

    修改完成loop流程设计，填充loop遍历循环的空间变量，优化循环逻辑。

--- src/engine/scheduler/process/loop.py
+++ src/engine/scheduler/process/loop.py
@@ -5,16 +5,49 @@ import json
 from engine.scheduler.process.type_process import TypeProcess
 
 class Loop(TypeProcess):
+    # 从配置文件或Task获取任务内所有循环流程最大循环次数，暂定1000
+    MAX_LOOP_COUNT = 1000
 ############## 运行时相关逻辑 ##############
     def process(self):
         execution = self.process_instance.template["execution"]
         loop_type = execution["loop_type"]
-        max_iterations = execution.get("max", 100)
+
+        # 获取最大循环次数，是任务（配置文件）限制，循环体模板限制的最小值
+        max_loop_num = min(execution.get("max", 100), self.MAX_LOOP_COUNT)
+
+        count_num = 0
+        iterable = list()
+        iterable_name = ""
+        # 计数循环情况的循环次数获取
+        if loop_type == "count":
+            count_value = execution["count"]["value"]
+            if execution["count"]["value_type"] == "variable":
+                count_num = self.process_instance.parameters.get(count_value, 0)
+            elif execution["count"]["value_type"] == "constant":
+                count_num = count_value
+            if not isinstance(count_num, int) or count_num < 0:
+                raise RuntimeError(f"循环次数参数 {count_value} 必须是大于0的整数。")
+        # 遍历循环情况的遍历列表和循环次数获取
+        elif loop_type == "iterate":
+            iterable_value = execution["iterable"]["value"]
+            if execution["iterable"]["value_type"] == "variable":
+                iterable = self.process_instance.parameters.get(iterable_value, [])
+                iterable_name = iterable_value + "_each_value"
+            elif execution["iterable"]["value_type"] == "constant":
+                iterable = iterable_value
+                iterable_name = "_iterable_each_value"
+            if not isinstance(iterable, list):
+                raise RuntimeError(f"循环遍历参数类型错误，必须是一个列表。")
+            count_num = len(iterable)
+
+        # 打印流程开始记录
         parameters_json = json.dumps(self.process_instance.parameters, ensure_ascii=False)
         self.process_instance.runtime_log.add_record(f"循环流程开始，循环类型: {loop_type}，当前流程空间变量: {parameters_json}。")
-        
+
+        # 满足初始条件循环次数小于循环最大值
         iteration_count = 0
-        while iteration_count < max_iterations:
+        while iteration_count < max_loop_num:
+            # 如果是条件循环，判断条件满足继续，否则跳出循环
             if loop_type == "condition":
                 try:
                     ret = self.evaluate_condition(execution["condition"])
@@ -26,26 +59,25 @@ class Loop(TypeProcess):
                 if not ret:
                     break
             elif loop_type == "count":
-                count_value = execution["count"]["value"]
-                if execution["count"]["value_type"] == "variable":
-                    count_value = self.process_instance.parameters.get(count_value, 0)
-                if iteration_count >= count_value:
+                if iteration_count >= count_num:
                     break
+            # 如果是遍历循环，判断循环次数是否小于遍历变量长度，否则跳出循环，并且设置循环变量到程序内存空间
             elif loop_type == "iterate":
-                iterable_value = execution["iterable"]["value"]
-                if execution["iterable"]["value_type"] == "variable":
-                    iterable_value = self.process_instance.parameters.get(iterable_value, [])
-                if iteration_count >= len(iterable_value):
+                if iteration_count >= count_num:
                     break
                 # 设置遍历数组参数值
-                self.process_instance.parameters.set("iterable_each_value",iterable_value[iteration_count])
+                self.process_instance.parameters[iterable_name] = iterable[iteration_count]
             
+            # 设置循环次数变量
             iteration_count += 1
+            self.process_instance.parameters["_loop_count"] = iteration_count
+            # 打印执行前log
             parameters_json = json.dumps(self.process_instance.parameters, ensure_ascii=False)
-            self.process_instance.runtime_log.add_record(f"第{iteration_count}次执行循环，当前流程空间变量: {parameters_json}。")
+            self.process_instance.runtime_log.add_record(f"循环体即将第 {iteration_count} 次执行，当前流程空间变量: {parameters_json}。")
+            # 执行循环体call
             self.process_instance._call_execute(execution["call"])
             
-        
+        # 执行完毕打印记录
         parameters_json = json.dumps(self.process_instance.parameters, ensure_ascii=False)
         self.process_instance.runtime_log.add_record(f"循环流程执行完毕，当前流程空间变量: {parameters_json}。")
 
